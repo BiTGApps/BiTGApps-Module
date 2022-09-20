7d8247e5801427ff003815198e4c7a2c25a00910
TheHitMan7 <krtik.vrma@gmail.com>
2022-09-20 22:39:43 +0530

    Minor Manager Changes
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- manager.sh
+++ manager.sh
@@ -4,11 +4,11 @@
 CURRENT='v1.2'
 RELEASE='v1.3'
 # Release Version
-OLDVERN='12'
-CURVERN='13'
+OLD_VER='12'
+CUR_VER='13'
 
 # Update JSON
 sed -i -e "s/$CURRENT/$RELEASE/g" */module.json
-sed -i -e "s/$OLDVERN/$CURVERN/g" */module.json
+sed -i -e "s/$OLD_VER/$CUR_VER/g" */module.json
 sed -i -e "s/$CURRENT/$RELEASE/g" */*/module.json
-sed -i -e "s/$OLDVERN/$CURVERN/g" */*/module.json
+sed -i -e "s/$OLD_VER/$CUR_VER/g" */*/module.json
