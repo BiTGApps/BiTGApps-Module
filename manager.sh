482c6d97bb7a122bb5a6655caeef529fd20bcff7
TheHitMan7 <krtik.vrma@gmail.com>
2022-10-09 13:33:35 +0530

    Update JSON Manager
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- manager.sh
+++ manager.sh
@@ -1,11 +1,11 @@
 # This file is part of The BiTGApps Project
 
 # General Revision
-CURRENT='v1.3'
-RELEASE='v1.4'
+CURRENT='v1.4'
+RELEASE='v1.5'
 # Release Version
-OLD_VER='"13",'
-CUR_VER='"14",'
+OLD_VER='"14",'
+CUR_VER='"15",'
 
 # Update JSON
 sed -i -e "s/$CURRENT/$RELEASE/g" */module.json
